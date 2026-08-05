6c3cb1223ce5117d37523bd80dbae4051ef77e46
chubin <befanyelse@gmail.com>
2026-08-05 21:00:27 +0900

    feat: 메인 레이아웃 추가

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,3 +1,36 @@
 export default function MainLayout() {
   return <div>MainLayout</div>;
 }
+import { Outlet } from "react-router-dom";
+
+import AppHeader from "../components/navigation/AppHeader";
+import BottomNavigation from "../components/navigation/BottomNavigation";
+
+function MainLayout() {
+  return (
+    <div
+      className="
+        mx-auto
+        grid
+        h-dvh
+        w-full
+        max-w-[480px]
+        grid-rows-[auto_minmax(0,1fr)_auto]
+        overflow-hidden
+        bg-surface
+        md:border-x
+        md:border-border
+      "
+    >
+      <AppHeader />
+
+      <main className="min-h-0 overflow-y-auto overscroll-y-contain px-5 py-6">
+        <Outlet />
+      </main>
+
+      <BottomNavigation />
+    </div>
+  );
+}
+
+export default MainLayout;
