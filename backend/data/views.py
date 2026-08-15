e170e909c69cad6242dfbf883231bb3150f29f6c
Seo yeon Kim <merock2tar@gmail.com>
2026-08-15 20:14:24 +0900

    feat: data 앱 sound/relaxtion/feedback 삭제 로직 추가 #35

--- backend/data/views.py
+++ backend/data/views.py
@@ -6,6 +6,9 @@ from rest_framework.views import APIView
 from accounts.models import AnonymousUser
 from checkin.models import CheckinRecord
 from tinnitus.models import PitchMatchSession, TinnitusProfile
+from sound.models import SoundSession
+from relaxtion.models import RelaxationSession
+from feedback.models import NightlyEvaluation
 
 
 # F-1201: 연결된 데이터 상태 조회
@@ -27,7 +30,6 @@ class HealthDisconnectView(APIView):
 
 
 # F-1205: 전체 데이터 삭제
-# (uuid는 유지, 각 앱의 기록만 삭제 - REQ-F-26 기준)
 class DeleteAllDataView(APIView):
 
     def post(self, request, uuid):
@@ -37,7 +39,14 @@ class DeleteAllDataView(APIView):
         deleted_counts["checkin"], _ = CheckinRecord.objects.filter(user=user).delete()
         deleted_counts["tinnitus_profile"], _ = TinnitusProfile.objects.filter(user=user).delete()
         deleted_counts["pitch_match_sessions"], _ = PitchMatchSession.objects.filter(user=user).delete()
-        # TODO: sound, feedback, personalization 앱 완성되면 여기에 삭제 로직 추가
+
+        deleted_counts["sound_sessions"], _ = SoundSession.objects.filter(user=user).delete()
+
+        deleted_counts["relaxation_sessions"], _ = RelaxationSession.objects.filter(user=user).delete()
+
+        deleted_counts["nightly_evaluations"], _ = NightlyEvaluation.objects.filter(user=user).delete()
+
+        # TODO: personalization 앱 완성되면 여기에 삭제 로직 추가
 
         user.health_data_connected = False
         user.save(update_fields=["health_data_connected"])
