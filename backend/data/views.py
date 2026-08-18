b79bec67c39cd7c50236e8bbc110398880cd84f4
Seo yeon Kim <merock2tar@gmail.com>
2026-08-18 10:12:00 +0900

    feat: data 앱 전체 삭제 구현 #35

--- backend/data/views.py
+++ backend/data/views.py
@@ -6,9 +6,11 @@ from rest_framework.views import APIView
 from accounts.models import AnonymousUser
 from checkin.models import CheckinRecord
 from tinnitus.models import PitchMatchSession, TinnitusProfile
+from soundfit.models import SoundFitProfile, SoundFitSession
 from sound.models import SoundSession
 from relaxtion.models import RelaxationSession
 from feedback.models import NightlyEvaluation
+from personalization.models import InterventionDecision, UserPersonalizationProfile
 
 
 # F-1201: 연결된 데이터 상태 조회
@@ -39,14 +41,13 @@ class DeleteAllDataView(APIView):
         deleted_counts["checkin"], _ = CheckinRecord.objects.filter(user=user).delete()
         deleted_counts["tinnitus_profile"], _ = TinnitusProfile.objects.filter(user=user).delete()
         deleted_counts["pitch_match_sessions"], _ = PitchMatchSession.objects.filter(user=user).delete()
-
+        deleted_counts["soundfit_profile"], _ = SoundFitProfile.objects.filter(user=user).delete()
+        deleted_counts["soundfit_sessions"], _ = SoundFitSession.objects.filter(user=user).delete()
         deleted_counts["sound_sessions"], _ = SoundSession.objects.filter(user=user).delete()
-
         deleted_counts["relaxation_sessions"], _ = RelaxationSession.objects.filter(user=user).delete()
-
         deleted_counts["nightly_evaluations"], _ = NightlyEvaluation.objects.filter(user=user).delete()
-
-        # TODO: personalization 앱 완성되면 여기에 삭제 로직 추가
+        deleted_counts["intervention_decisions"], _ = InterventionDecision.objects.filter(user=user).delete()
+        deleted_counts["personalization_profile"], _ = UserPersonalizationProfile.objects.filter(user=user).delete()
 
         user.health_data_connected = False
         user.save(update_fields=["health_data_connected"])
